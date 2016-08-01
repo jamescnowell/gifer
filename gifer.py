7d1095995b2feac074b54b1cf6e6659311bc010e
Kenny Dewhirst <kenny.dewhirst@gmail.com>
2016-08-01 15:46:04 -0400

    Simplified URL building and added a shebang line

--- gifer.py
+++ gifer.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 import webbrowser
 import sys
-url = r'https://www.google.com/search?espv=2&biw=1280&bih=1303&tbs=itp%3Aanimated&tbm=isch&sa=1&q=REPLACE_STRING&oq=REPLACE_STRING&gs_l=img.3..0l10.22603.150807.0.151047.14.10.1.3.3.0.129.651.9j1.10.0....0...1c.1.64.img..0.14.661...0i30j0i5i30.abGf33oteBo'
+url = r'https://www.google.com/search?tbm=isch&tbs=itp:animated&q='
 args = sys.argv[1:]
-test = url.replace('REPLACE_STRING', '+'.join(args))
+test = url + '+'.join(args)
 webbrowser.open(test)
